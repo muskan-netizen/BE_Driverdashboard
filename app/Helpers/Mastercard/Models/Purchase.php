813b221a1f8ab69ddf6dcf3b452a6388eef6b561
Shubhanshu Tomar <shubhanshucbl24@gmail.com>
2024-05-02 19:39:11 +0530

    feat(mastercard): implement mastercard for dispatcher

---
+++ app/Helpers/Mastercard/Models/Purchase.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Helpers\Mastercard\Models;
+
+use App\Helpers\Mastercard\Interaction as IntrOp;
+
+final class Purchase implements Model
+{
+    private object $object;
+    private Order $order;
+    private Interaction $interaction;
+
+    public function __construct(string $merchant_id)
+    {
+        $this->interaction = (new Interaction(IntrOp::PURCHASE))
+            ->setMerchantId($merchant_id);
+
+        $this->object = (object)[
+            'transaction' => (object)[
+                'reference' => time() . rand(00000, 99999),
+            ],
+        ];
+    }
+
+    /**
+     * Retrieves the active instance of an `Interaction` Model
+     *
+     * @return Interaction
+     */
+    public function getInteraction(): Interaction
+    {
+        return $this->interaction;
+    }
+
+    public function getOrder(): ?Order
+    {
+        if (!isset($this->order)) return null;
+
+        return $this->order;
+    }
+
+    /**
+     * Set order model metadata
+     *
+     * @param Order $order
+     */
+    public function setOrder(Order $order): self
+    {
+        $this->order = $order;
+        return $this;
+    }
+
+    /**
+     * Sets the customer model metadata
+     *
+     * @param Customer $customer
+     */
+    public function setCustomer(Customer $customer): self
+    {
+        $this->object->customer = $customer->toJson();
+        return $this;
+    }
+
+    public function toJson(): array
+    {
+        if (!isset($this->order)) throw new \Error("order must be set");
+        $this->object->interaction = $this->interaction->toJson();
+        $this->object->order = $this->order->toJson();
+
+        return (array)$this->object;
+    }
+};

8049890b5ad4e6bff38a52a0d68677ae1eb528ef
gaurav sandhu <gaurav.sandhu@codebrewinnovations.com>
2024-06-21 14:52:29 +0530

    changes

--- app/Services/FirebaseService.php
+++ app/Services/FirebaseService.php
@@ -114,14 +114,14 @@ class FirebaseService
         
                 // Process the data section, converting specific fields to strings
                 //$newData['data'] = [];
-                foreach ($data['data'] as $key => $value) {
-                    //if (in_array($key, ['order_id', 'order_status', 'redirect_type'])) {
-                        $message['data'][$key] = (string)$value;
-                        // $message[$key] = $value;
-                    //} else {
-                        //$newData['data'][$key] = $value;
-                    //}
-                }
+                // foreach ($data['data'] as $key => $value) {
+                //     //if (in_array($key, ['order_id', 'order_status', 'redirect_type'])) {
+                //         $message['data'][$key] = (string)$value;
+                //         // $message[$key] = $value;
+                //     //} else {
+                //         //$newData['data'][$key] = $value;
+                //     //}
+                // }
 
                 //$messages[] = $message;
 

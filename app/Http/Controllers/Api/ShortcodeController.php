4ddc9c933ebfb9a5bcbec0f1c5f315540ae87b41
Nishant <rajat.k@codebrewinnovations.com>
2024-07-02 15:30:31 +0530

    flutterwave payment gateway in progress

--- app/Http/Controllers/Api/ShortcodeController.php
+++ app/Http/Controllers/Api/ShortcodeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Api\BaseController;
-use App\Model\{Client,ClientPreference, Currency};
+use App\Model\{Client,ClientPreference, Currency, PaymentOption};
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Carbon\Carbon;
@@ -86,6 +86,9 @@ class ShortcodeController extends BaseController
                 'hold_to_complete',
             ]);
             if(!empty($client_db_data)){
+                $flutterwaveCreds = json_decode(PaymentOption::where('code','flutterwave')->first()->credentials);
+                $flutterPublicKey = $flutterwaveCreds->client_id;
+
                 $client->client_db_id = $client_db_data->id;
                 $client->client_db_code = $client_db_data->code;
                 $client->is_driver_slot = !empty($client_db_data->getPreference) && isset($client_db_data->getPreference->is_driver_slot) ? $client_db_data->getPreference->is_driver_slot : 0;
@@ -95,6 +98,8 @@ class ShortcodeController extends BaseController
                 $Currency = Currency::where('id', $client_db_data->getPreference->currency_id)->first();
                 $client['currencyCode'] = $Currency->symbol??null;
                 $client['distanceUnit'] = $client_db_data->getPreference->distance_unit??null;
+                $client['flutterwave_public_key'] = $flutterPublicKey;
+                
                 
             }
         }

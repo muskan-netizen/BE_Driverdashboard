4ed6596f6b4b5c65d53adf690d8bc3886101bbf2
avinash-kumar2-admin <avinash.kumar@codebrewinnovations.com>
2025-04-22 18:08:56 +0530

    remove logs

--- app/Jobs/RosterCreate.php
+++ app/Jobs/RosterCreate.php
@@ -33,7 +33,6 @@ class RosterCreate implements ShouldQueue
     }
     public function handle()
     {
-        \Log::info('Roster Create Job Started');
         try {
             $schemaName = 'royodelivery_db';
             $default = [
